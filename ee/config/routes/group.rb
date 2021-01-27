mehulagg/gitlab

Epic Boards - Add Boards navigation item under Epics

ee/config/routes/group.rb
@@ -113,6 +113,10 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
       end
 
       collection do
+        resources :boards, only: [:index, :show], as: 'epics_boards'
+      end
+
+      collection do
         post :bulk_update
       end
     end

ibinteractive/https-gitlab.com-gitlab-org-gitlab

ee/config/routes/group.rb
@@ -113,6 +113,10 @@ constraints(::Constraints::GroupUrlConstrainer.new) do
       end
 
       collection do
+        resources :boards, only: [:index, :show], as: 'epic_boards'
+      end
+
+      collection do
         post :bulk_update
       end
     end