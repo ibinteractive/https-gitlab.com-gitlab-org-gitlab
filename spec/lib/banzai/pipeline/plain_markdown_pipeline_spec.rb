a8f85680f1867f807cb764b1c275ddffe1fcec04
Brett Walker <bwalker@gitlab.com>
2021-01-29 11:24:06 -0600

    Add feature flag honor_escaped_markdown
    to allow us to turn off if there are significant
    performance issues

--- spec/lib/banzai/pipeline/plain_markdown_pipeline_spec.rb
+++ spec/lib/banzai/pipeline/plain_markdown_pipeline_spec.rb
@@ -17,6 +17,20 @@ RSpec.describe Banzai::Pipeline::PlainMarkdownPipeline do
       result
     end
 
+    context 'when feature flag honor_escaped_markdown is disabled' do
+      before do
+        stub_feature_flags(honor_escaped_markdown: false)
+      end
+
+      it 'does not escape the markdown' do
+        result = described_class.call(%q(\!), project: project)
+        output = result[:output].to_html
+
+        expect(output).to eq('<p data-sourcepos="1:1-1:2">!</p>')
+        expect(result[:escaped_literals]).to be_falsey
+      end
+    end
+
     # Test strings taken from https://spec.commonmark.org/0.29/#backslash-escapes
     describe 'CommonMark tests', :aggregate_failures do
       it 'converts all ASCII punctuation to literals' do
